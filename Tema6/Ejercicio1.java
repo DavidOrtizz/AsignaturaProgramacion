9c9b99e5b71e03bed5c0f28b51175f577fae8a01
David Ortiz Corchero <david140303@hotmail.es>
2022-12-16 17:20:43 +0100

    Ejercicios enviados

---
+++ Tema6/Ejercicio1.java
@@ -0,0 +1,24 @@
+/*Este programa está hecho por: David Ortiz Corchero
+*Curso: 1ºDAM Tarde
+*Descripción del programa: Un programa que que muestre la tirada de tres dados. Se debe mostrar
+*también la suma total (los puntos que suman entre los tres dados).
+*/
+
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    int suma;
+    int numero;
+
+    suma = 0;
+    System.out.println("Vas a hacer una tirada de un dado tres veces");
+    System.out.print("Has sacado un: ");
+
+    for (int cont = 1; cont <= 3; cont++) {
+      numero = ((int) (Math.random() * 6) + 1);
+      suma += numero; // La suma de los puntos
+      System.out.print(numero + " "); // Te muestra los numeros que has sacado
+    }
+    System.out.println();
+    System.out.println("Tus puntos totales son " + suma); // Muestra del total de los puntos
+  }
+}
