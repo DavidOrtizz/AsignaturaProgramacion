d30ad180b8ab76679048baeefc6a4658fe969bb1
David Ortiz Corchero <david140303@hotmail.es>
2023-02-09 19:33:06 +0100

    Arreglos

--- Tema8/prueba1.java
+++ Tema8/prueba1.java
@@ -1,10 +1,10 @@
-import misFunciones.Multiplicacion;
+import misFunciones.Multiplicacion.multiplica;
 
 public class prueba1 {
   public static void main(String[] args) {
     double a = 10;
     double b = 5;
 
-    System.out.println(Multiplicacion(a, b));
+    System.out.println(multiplica(a, b));
   }
 }
