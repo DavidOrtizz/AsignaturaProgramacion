797d7deb060b6facc63c34f2158725d1f3948686
David Ortiz Corchero <david140303@hotmail.es>
2023-03-18 17:39:28 +0100

    Ejercicios

--- Tema9/Arrays/Ejercicio2/ArrayGato2.java
+++ Tema9/Arrays/Ejercicio2/ArrayGato2.java
@@ -1,5 +1,6 @@
 public class ArrayGato2 {
   private String color;
+  private int comida = 10;
 
   public GatoArray(String color) {
     this.color = color;
@@ -11,5 +12,18 @@ public class ArrayGato2 {
 
   public void setColor(String color) {
     this.color = color;
+  }
+
+  public void come(int comida) {
+    if (comida != 0) {
+      this.comida += 1;
+      comida -= 1;
+      for (int i = 0; i <= this.comida; i++) {
+        System.out.print("miau "); // Por cada comida que haya comido hace el sonido miau
+      }
+      System.out.println(); // Salto de linea
+    } else {
+      System.out.println("Ya no hay comida"); // Cuando comida es 0
+    }
   }
 }
