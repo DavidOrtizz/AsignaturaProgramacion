fe5ee85cd8496b5fa44c94148c07348833e36701
David Ortiz Corchero <david140303@hotmail.es>
2023-03-20 16:30:22 +0100

    Modificación del ejercicio

--- Tema9/Arrays/Ejercicio2/ArrayGato2.java
+++ Tema9/Arrays/Ejercicio2/ArrayGato2.java
@@ -19,11 +19,13 @@ public class ArrayGato2 {
       this.comida += 1;
       comida -= 1;
       for (int i = 0; i <= this.comida; i++) {
-        System.out.print("miau "); // Por cada comida que haya comido hace el sonido miau
+        if (comida != 0) {
+          System.out.print("miau "); // Por cada comida que haya comido hace el sonido miau
+        } else {
+          System.out.println(" ya no hay comida"); // Cuando comida es 0
+        }
       }
       System.out.println(); // Salto de linea
-    } else {
-      System.out.println("Ya no hay comida"); // Cuando comida es 0
     }
   }
 }
